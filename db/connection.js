386baa728e7517f56c349ae4afbd48cd6b70c719
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-07 21:07:58 -0400

    deleted port

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 // Creating a MySQL connection
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3001,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "employee_db",
